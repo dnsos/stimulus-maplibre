fa4066a5cae84b0d140159f8867bf02789a38443
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-11-02 19:10:47 +0100

    feat: make basic map work

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
+import maplibregl, { Map } from "maplibre-gl";
 
 export default class extends Controller {
+  map: Map;
+
   connect(): void {
     console.log("Connected Maplibre controller");
+
+    this.map = new maplibregl.Map({
+      container: "map",
+      style: "https://demotiles.maplibre.org/style.json", // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 5, // starting zoom
+    });
   }
 
   greet(): void {
