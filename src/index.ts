0eed30d9705b3fcbfb2932239a2f400ea67a70ee
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2022-11-06 20:43:18 +0100

    feat: implement basic map config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,126 @@
 import { Controller } from "@hotwired/stimulus";
-import maplibregl, { Map } from "maplibre-gl";
+import maplibregl, { Map, StyleSpecification } from "maplibre-gl";
 
 export default class extends Controller {
-  map: Map;
+  declare map: Map;
+
+  declare styleUrlValue: string;
+  declare hasStyleUrlValue: boolean;
+
+  declare centerValue: string;
+  declare hasCenterValue: boolean;
+
+  declare zoomValue: number;
+  declare hasZoomValue: boolean;
+
+  declare minZoomValue: number;
+  declare maxZoomValue: number;
+
+  declare interactiveValue: boolean;
+
+  static values = {
+    center: { type: String, default: "13.404954,52.520008" },
+    zoom: { type: Number, default: 11 },
+    minZoom: { type: Number, default: 0 },
+    maxZoom: { type: Number, default: 22 },
+    styleUrl: String,
+    interactive: { type: Boolean, default: true },
+  };
 
   connect(): void {
-    console.log("Connected Maplibre controller");
+    this.checkContainerDimensions();
+    const hasValidCenter = this.hasValidCenter();
+
+    if (!hasValidCenter) return;
 
     this.map = new maplibregl.Map({
-      container: "map",
-      style: "https://demotiles.maplibre.org/style.json", // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 5, // starting zoom
+      container: this.element as HTMLElement,
+      style: this.mapStyle,
+      center: [this.longitude, this.latitude],
+      zoom: this.zoomValue,
+      minZoom: this.minZoomValue,
+      maxZoom: this.maxZoomValue,
+      interactive: this.interactiveValue,
     });
   }
 
-  greet(): void {
-    console.log("Hey from the map!");
+  get mapStyle(): string | StyleSpecification {
+    return this.hasStyleUrlValue && this.styleUrlValue.length > 0
+      ? this.styleUrlValue
+      : this.defaultMapStyle;
+  }
+
+  get defaultMapStyle(): StyleSpecification {
+    return {
+      version: 8,
+      sources: {
+        osm: {
+          type: "raster",
+          tiles: ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+          tileSize: 256,
+          attribution: "&copy; OpenStreetMap Contributors",
+          maxzoom: 19,
+        },
+      },
+      layers: [
+        {
+          id: "osm",
+          type: "raster",
+          source: "osm", // This must match the source key above
+        },
+      ],
+    };
+  }
+
+  zoomValueChanged(newZoomValue: number, previousZoomValue: number): void {
+    if (newZoomValue !== previousZoomValue) {
+      if (!this.map) return;
+      this.map.easeTo({
+        zoom: newZoomValue,
+      });
+    }
+  }
+
+  centerValueChanged(
+    newCenterValue: number,
+    previousCenterValue: number
+  ): void {
+    if (newCenterValue !== previousCenterValue) {
+      const hasValidCenter = this.hasValidCenter();
+      if (!this.map || !hasValidCenter) return;
+      this.map.easeTo({
+        center: [this.longitude, this.latitude],
+      });
+    }
+  }
+
+  checkContainerDimensions(): void {
+    if (this.element.clientWidth === 0 || this.element.clientHeight === 0) {
+      console.warn(
+        "The map is not visible because either the map container's height or width is zero."
+      );
+    }
+  }
+
+  hasValidCenter(): boolean {
+    const [longitude, latitude] = this.centerValue.split(",");
+    const parsedLongitude = parseFloat(longitude);
+    const parsedLatitude = parseFloat(latitude);
+    if (!parsedLongitude || !parsedLatitude) {
+      console.error(
+        "Please provide valid longitude and latitude in the shape of `13.123456,52.123456` for the center value"
+      );
+      return false;
+    }
+    return true;
+  }
+
+  get latitude(): number {
+    const [, latitude] = this.centerValue.split(",");
+    return parseFloat(latitude);
+  }
+  get longitude(): number {
+    const [longitude] = this.centerValue.split(",");
+    return parseFloat(longitude);
   }
 }
